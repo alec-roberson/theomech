e559ebe80bc1f9936e003f0723a41076ab2d5236
alec-roberson <alectroberson@gmail.com>
2022-12-16 08:54:29 -0800

    update

--- simulation.py
+++ simulation.py
@@ -4,15 +4,13 @@ import matplotlib.pyplot as plt
 
 x0 = 1
 omega = 10
-g = 9.81
+g = 1
 l = 1
 
-TAU_MAX = 100
+TIME_MAX = 100
 INTERVALS = 10000
-dt = TAU_MAX / (omega * INTERVALS)
 
-taus = np.linspace(0, TAU_MAX, INTERVALS)
-times = taus/omega
+times = np.linspace(0, TIME_MAX, INTERVALS)
 
 # initial_states = [np.array([0,0])]
 initial_states = []
@@ -29,7 +27,7 @@ plt.figure(figsize=(8,8))
 
 for s0 in initial_states:
     sol = solve_ivp(func, [0, TAU_MAX], s0, t_eval=taus)
-    color =  np.random.rand(3) # [1,0,0] #
+    color =  np.random.rand(3)
     plt.subplot(311)
     plt.plot(times, sol.y[0], color=color)
     plt.subplot(312)
